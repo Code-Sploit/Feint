d0b352034c9e318098fb87c29f8093558fca0c90
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-11-21 10:31:26 -0600

    Add parser (header)

---
+++ parser.h
@@ -0,0 +1,22 @@
+#include "lexer.h"
+
+typedef struct SCOPE_NODE_STRUCT {
+    char *_type;
+    char *_name;
+    char *_oper;
+    char *_value;
+    char *_otype;
+} Scope_Node;
+
+typedef struct SCOPE_STRUCT {
+    char *_name;
+
+    int _T_count;
+
+    Scope_Node *Tokens[256];
+} Scope_T;
+
+Scope_T *ParserCompile(Lexer_T *lexer, int _debug);
+
+Scope_T *InitializeScope(char *_name, Scope_Node **_Tokens);
+Scope_Node *InitializeScopeNode(char *_type, char *_name, char *_oper, char *_value, char *_otype);
