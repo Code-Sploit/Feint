fb11d721646e447280810c34853dba80bcfe66e6
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-11-21 10:06:14 -0600

    BIG REWRITE

--- ast.c
+++ ast.c
@@ -1,89 +1,126 @@
 #include "ast.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include <ctype.h>
 
-void InvalidSyntax(char syn, int _ret)
+int Compare(char *_e1, char *_e2)
 {
-    printf("Critical error: ['%c'] -> undefined syntax.\n", syn);
+    if (strcmp(_e1, _e2) == 0) {return 1;}
+    if (strcmp(_e1, _e2) != 0) {return 0;}
 
-    exit(_ret);
+    return 0;
 }
 
-AST_Node *InitializeASTNode(char *value, int type)
+void WriteASMTo(char *_out, char *_data)
 {
-    AST_Node *Node = calloc(1, sizeof(AST_Node));
+    FILE *fp = fopen(_out, "w+");
 
-    Node->value = value;
-    Node->Type  = type;
-
-    return Node;
-}
-
-AST_Node *InitializeASTFunc(char *_src, char *_dest, char *_arg, int type)
-{
-    AST_Node *Node = calloc(1, sizeof(AST_Node));
-
-    Node->value = _src;
-    Node->_dest = _dest;
-    Node->_arg  = _arg;
-
-    Node->Type  = type;
-
-    return Node;
-}
-
-AST_Node *ASTGetNextNode(Lexer_T *lexer)
-{
-    LexerSkipWhitespace(lexer);
-
-    while (LexerPeekOffset(lexer, 1) != '\0')
+    if (!fp)
     {
-        //printf("S: %d A: %d D: %d\n", isspace(lexer->c), isalpha(lexer->c), isdigit(lexer->c));
+        printf("Error could not open '%s' for output asm code!\n");
 
-        if (lexer->c == 13 || lexer->c == 10 || lexer->c == ' ' || lexer->c == '\t') {LexerAdvanceCharacter(lexer); continue;}
+        exit(1);
+    }
 
-        if (isalpha(lexer->c) != 0) {return InitializeASTNode(LexerAdvanceWithId(lexer), TOKEN_ID);}
-        if (isdigit(lexer->c) != 0) {return InitializeASTNode(LexerAdvanceWithDigit(lexer), TOKEN_DIGIT);}
+    fputs(_data, fp);
+    fclose(fp);
 
-        if (isspace(lexer->c) == 0 && isalpha(lexer->c) == 0 && isdigit(lexer->c) == 0)
+    char *_nasm_cmd = calloc(1, sizeof(char));
+    char *_link_cmd = calloc(1, sizeof(char));
+
+    _nasm_cmd = realloc(_nasm_cmd, (strlen(_nasm_cmd) + strlen("nasm -f elf64 -o output.out output.s") + 1));
+    _link_cmd = realloc(_link_cmd, (strlen(_link_cmd) + strlen("ld output.out -o output") + 1));
+
+    strcat(_nasm_cmd, "nasm -f elf64 -o output.out output.s");
+    strcat(_link_cmd, "ld output.out -o output");
+
+    int _nasm = system(_nasm_cmd);
+    int _link = system(_link_cmd);
+
+    if (_nasm == 1) {printf("\n\nError while running NASM x86 Assemble of Machine Code!\n");}
+    if (_link == 1) {printf("\n\nError while running LD of Assembled output!\n");}
+}
+
+void ASTGenerateMachineCode(Scope_T *_Scope, int _debug)
+{
+    printf("\n\n");
+
+    char *template = calloc(1, sizeof(char));
+
+    template = realloc(template, (strlen(template) + strlen("global _start\n\nsection .text\n\n_start:\nmov rax, 60\nmov rdi, 0\nsyscall") + 1));
+
+    strcat(template, "global _start\n\nsection .text\n\n_start:\nmov rax, 60\nmov rdi, 0\nsyscall");
+
+    int _LastToken = 0;
+
+    for (int i = 0; i < _Scope->_T_count; i++)
+    {
+        char *val   = _Scope->Tokens[i]->_value;
+        char *oper  = _Scope->Tokens[i]->_oper;
+        char *name  = _Scope->Tokens[i]->_name;
+        char *type  = _Scope->Tokens[i]->_type;
+        char *otype = _Scope->Tokens[i]->_otype;
+
+        if (_debug) {printf("V: '%s' O: '%s' N: '%s' T: '%s' O: '%s'\n", val, oper, name, type, otype);}
+
+        if (Compare(otype, "STATEMENT"))
         {
-            switch (lexer->c)
+            _LastToken = 0;
+
+            if (Compare(name, "return"))
             {
-                case '!': {LexerAdvanceCharacter(lexer); return InitializeASTNode("!", TOKEN_EXCL);}
-                case '@': {LexerAdvanceCharacter(lexer); return InitializeASTNode("@", TOKEN_AT);}
-                case '#': {LexerAdvanceCharacter(lexer); return InitializeASTNode("#", TOKEN_HASHTAG);}
-                case '$': {LexerAdvanceCharacter(lexer); return InitializeASTNode("$", TOKEN_DOLLAR);}
-                case '%': {LexerAdvanceCharacter(lexer); return InitializeASTNode("%", TOKEN_PERCENT);}
-                case '^': {LexerAdvanceCharacter(lexer); return InitializeASTNode("^", TOKEN_UP);}
-                case '&': {LexerAdvanceCharacter(lexer); return InitializeASTNode("&", TOKEN_ZS);}
-                case '*': {LexerAdvanceCharacter(lexer); return InitializeASTNode("*", TOKEN_STAR);}
-                case '(': {LexerAdvanceCharacter(lexer); return InitializeASTNode("(", TOKEN_LPAREN);}
-                case ')': {LexerAdvanceCharacter(lexer); return InitializeASTNode(")", TOKEN_RPAREN);}
-                case '+': {LexerAdvanceCharacter(lexer); return InitializeASTNode("+", TOKEN_PLUS);}
-                case '=': {LexerAdvanceCharacter(lexer); return InitializeASTNode("=", TOKEN_IS);}
-                case '{': {LexerAdvanceCharacter(lexer); return InitializeASTNode("{", TOKEN_RBRACE);}
-                case '}': {LexerAdvanceCharacter(lexer); return InitializeASTNode("}", TOKEN_LBRACE);}
-                case ';': {LexerAdvanceCharacter(lexer); return InitializeASTNode(";", TOKEN_SEMICOLON);}
-
-                case '"': {LexerAdvanceCharacter(lexer); return InitializeASTNode(LexerGetVal(lexer, '"'), TOKEN_VAL);}
-                case '-': {
-                    if (LexerPeekOffset(lexer, 1) != '>') {LexerAdvanceCharacter(lexer); return InitializeASTNode("-", TOKEN_MINUS);}
-
-                    char *_src  = LexerGetFunctionSrc(lexer);
-                    char *_dest = LexerGetFunctionDest(lexer);
-                    char *_arg  = LexerGetFunctionArg(lexer);
-
-                    return InitializeASTFunc(_src, _dest, _arg, TOKEN_BFUNC);
-                }
-
-                default: InvalidSyntax(lexer->c, 1);
+                /* Return statement */
             }
         }
 
-        LexerAdvanceCharacter(lexer);
+        if (Compare(otype, "EXPR"))
+        {
+            if (_LastToken != 1)
+            {
+                template = realloc(template, strlen(template) + strlen("\nsection .rodata\n") + 1);
+
+                strcat(template, "\nsection .rodata\n");
+            }
+
+            _LastToken = 1;
+
+            if (Compare(type, "int"))
+            {
+                /* Integer definition */
+                template = realloc(template, (strlen(template) + strlen(name) + strlen(": db \"") + strlen(val) + strlen("\"") + 1));
+
+                strcat(template, name);
+                strcat(template, ": db \"");
+                strcat(template, val);
+                strcat(template, "\"");
+                strcat(template, "\n");
+
+                if (_debug) {printf("%s\n", template);}
+            }
+
+            if (Compare(type, "str"))
+            {
+                /* String definition */
+                template = realloc(template, (strlen(template) + strlen(name) + strlen(": db \"") + strlen(val) + strlen("\"") + 1));
+
+                strcat(template, name);
+                strcat(template, ": db \"");
+                strcat(template, val);
+                strcat(template, "\"");
+                strcat(template, "\n");
+
+                if (_debug) {printf("%s\n", template);}
+            }
+        }
+
+        if (!Compare(otype, "EXPR") && !Compare(otype, "STATEMENT"))
+        {
+            printf("Critical error: [Invalid expression in ASM Generation!]\n");
+
+            exit(1);
+        }
     }
 
-    return InitializeASTNode("\0", TOKEN_EOF);
+    WriteASMTo("output.s", template);
 }
