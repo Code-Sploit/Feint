9c20465e049e71098d94f41ee5e1c7d5fe35c53f
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-11-21 10:08:54 -0600

    BIG REWRITE

--- lexer.h
+++ lexer.h
@@ -1,3 +1,5 @@
+#include "token.h"
+
 typedef struct LEXER_STRUCT {
     char *src;
     char c;
@@ -9,14 +11,10 @@ Lexer_T *InitializeLexer(char *src);
 
 char LexerPeekOffset(Lexer_T *lexer, int _offset);
 
-char *LexerGetVal(Lexer_T *lexer, char _delim);
 char *LexerAdvanceWithId(Lexer_T *lexer);
 char *LexerAdvanceWithDigit(Lexer_T *lexer);
 
-char *LexerGetFunctionSrc(Lexer_T *lexer);
-char *LexerGetFunctionDest(Lexer_T *lexer);
-char *LexerGetFunctionArg(Lexer_T *lexer);
-
 void LexerSkipWhitespace(Lexer_T *lexer);
-void LexerGenerateAST(Lexer_T *lexer);
 void LexerAdvanceCharacter(Lexer_T *lexer);
+
+Token_Node *LexerGetNextToken(Lexer_T *lexer);
