4e40ff48bc4117dd602c3f969aa8bf752ed528b2
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-11-22 08:44:44 -0600

    Add functions

--- ast.h
+++ ast.h
@@ -2,4 +2,4 @@
 
 void ASTGenerateMachineCode(Scope_T *_Scope, int _debug);
 
-void WriteASMTo(char *_out, char *_start_function_data, char *_declarations);
+void WriteASMTo(char *_out, char *_start_function_data, char *_declarations, char **_functions, int _F_Count);
