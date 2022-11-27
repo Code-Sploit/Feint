b60a5ae1a9a80189ae13e29e1243f281aa493e57
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-11-27 11:25:17 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,50 @@
 #include "feint.h"
 
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-void CompilationSuccessfull(char *File)
+void CompilationSuccessfull(char *File, char *OFile)
 {
-    printf("\nCompilation of: [%s] succeeded!\n", File);
+    if (strcmp(OFile, "NULL") == 0)
+    {
+        printf("\nCompilation of: [\033[0;36m%s\033[0m] \033[0;32msucceeded\033[0m!\n\nSaved executable file to: [\033[0;36m./output\033[0m]", File);
+    }
+    else
+    {
+        printf("\nCompilation of: [\033[0;36m%s\033[0m] \033[0;32msucceeded\033[0m!\n\nSaved executable file to: [\033[0;36m./%s\033[0m]", File, OFile);
+    }
 }
 
 void CompilationUnSuccessfull(char *File)
 {
-    printf("\nCompilation of: [%s] failed!\n", File);
+    printf("\nCompilation of: [\033[0;36m%s\033[0m] \033[0;31mfailed\033[0m!\n", File);
 }
 
 int main(int argc, char** argv)
 {
-    if (argc < 3)
+    if (argc < 4)
     {
         printf("Error: Invalid argument count!\n");
-        printf("Syntax: feintc <input file> [compiler args]\n");
+        printf("Syntax: feintc <input file> -o <output file>\n");
 
         return -1;
     }
 
-    int ret = FeintCompile(argv[1], argv[2], 0);
+    char *_IFile = argv[1];
+    char *_OFile = argv[3];
+    char *__D    = argv[4];
 
-    if (ret == 0) {CompilationSuccessfull(argv[1]); return 0;}
-    if (ret == -1) {CompilationUnSuccessfull(argv[1]); return 1;}
+    int __ret = FeintCompile(_IFile, __D, _OFile, 0);
+
+    if (__ret == 0)
+    {
+        CompilationSuccessfull(_IFile, _OFile);
+    }
+    else
+    {
+        CompilationUnSuccessfull(_IFile);
+    }
 
     return 0;
 }
